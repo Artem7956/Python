e8b84a1e03978765b3dd5a3f43f66bb830a23d41
Artem7956 <artamon78@yandex.ru>
2018-05-24 20:48:36 +0300

    no message

--- classes/animals.py
+++ classes/animals.py
@@ -1,48 +1,81 @@
 class animal:
     voice = ''
+    ration=''
     def make_voice(self):
         print (self.voice)
         
     pass
 class bird(animal):
+    ration='Зерно'
     fly = None
     pass
 class mammals(animal):
+    ration='Трава'
     pass
 class cow(mammals):
-    voice='Moooo'
+    voice='Му'
+    
+    def get_milk(self,r,c):# r-рацион,c-количество травы. Возвращает n - литров молока по формуле n=c/2
+        if r==self.ration: 
+            return c/2
+        else:
+            return 'Я не ем '+ r
+
+        
+
+    
     pass
 class sheep(mammals):
-    voice='beee'
+    voice='Бееее'
     max_wool=5
     def get_wool(self,c):
         if c<=self.max_wool:
-            return 'Here is '+ str(c) +'kilo of wool'
+            return 'Вот '+ str(c) +'килограмм шерсти'
         else:
-            return "I have not "+ str(c) + ' kilo of wool,here is only '+str(self.max_wool)+' kilo'  
+            return "У меня нет "+ str(c) + ' килограмм шерсти. Вот только '+str(self.max_wool)+' килограмм'  
         
     pass
 class pig(mammals):
-    voice='Hriu'
+    voice='Хрю'
     pass
 class goat(mammals):
-    voice='Mmmmee'
+    voice='Меее'
     pass
 class duck(bird):
-    voice='KriaKria'
+    voice='Кря'
     fly=True
     pass
 class chicken(bird):
-    voice='Kukarekooo'
+    voice='Кукареку'
+    eggs=0
+    
     fly=False
+    def get_egg(self,c):
+        if self.eggs==0 :
+            self.eggs+=1
+            return 1
+        else: return "Я сегодня уже давала яйца"
+        
+    
     pass
 class gees(bird):
-    voice='Gagaga'
+    voice='Гага'
     fly=True
     pass
+d1=duck()
+d1.make_voice()
+c1=cow()
+print('Получено молока,л:',c1.get_milk('Трава',10))
+print(c1.get_milk('Покрышки',10))
+ch=chicken()
+print(ch.get_egg(1))
+print(ch.get_egg(1))
+
 
 s1=sheep()
-print(s1.get_wool(7))
+print(s1.get_wool(4))
+
+print(s1.get_wool(20))
 
 
 
