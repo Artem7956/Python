c59a64d462542e78d5f466271f37c13b3d41df46
Artem7956 <artamon78@yandex.ru>
2018-06-26 20:45:08 +0300

    no message

--- XML and SOAP/temps.py
+++ XML and SOAP/temps.py
@@ -1,11 +1,12 @@
 import requests
-import osa
+import xml.dom.minidom
+
 
 def get_cel_by_far(temp):
 
     url = 'https://www.w3schools.com/xml/tempconvert.asmx'
-    headers = {"Content-Type":"text/xml"}
-    data_f_to_c = f''''<?xml version="1.0" encoding="utf-8"?>
+    headers = {"Content-Type": "text/xml"}
+    data_f_to_c = f'''<?xml version="1.0" encoding="utf-8"?>
 <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
 xmlns:xsd="http://www.w3.org/2001/XMLSchema" 
 xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
@@ -15,21 +16,19 @@ xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
     </FahrenheitToCelsius>
   </soap:Body>
 </soap:Envelope>'''
-    print(data_f_to_c)
-    req = requests.post(url=url,data=data_f_to_c,headers=headers)
-    print ('result',req.text)
+
+    req = requests.post(url=url, data=data_f_to_c, headers=headers)
+    dom = xml.dom.minidom.parseString(req.text)
+    dom.normalize()
+    node1 = dom.getElementsByTagName("FahrenheitToCelsiusResult")[0]
+    return node1.childNodes[0].nodeValue
 
 
 def get_avg_temp(file):
-
     with open(file, 'r') as f:
-        temps = list()
-        for line in f:
-             temps.append(int(line.split()[0]))
-
+        temps = [int(line.split()[0]) for line in f]
     return sum(temps)/len(temps)
 
-avg = get_avg_temp('temps.txt')
-print(avg)
-get_cel_by_far(120)
 
+avg = get_avg_temp('temps.txt')
+print('Средняя температура по Фаренгейту: {0}, по Цельсию: {1}'.format(avg, get_cel_by_far(avg)))
